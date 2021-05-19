22183aa3f86de94f79a8975064d03ca10bb6d667
JonaEspinales <79060601+JonaEspinales@users.noreply.github.com>
2021-05-19 00:22:55 -0500

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,7 @@
+const Nombre = document.getElementById("name")
+const ID = document.getElementById("name")
+const Apellido = document.getElementById("name")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
