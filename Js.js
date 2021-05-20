4e9128a772024014b4a7ba14b40e53a3a2035ca1
JonaEspinales <79060601+JonaEspinales@users.noreply.github.com>
2021-05-20 00:00:42 -0500

    Datos nuevos

--- Js.js
+++ Js.js
@@ -1,7 +1,49 @@
-const Nombre = document.getElementById("name")
-const ID = document.getElementById("name")
-const Apellido = document.getElementById("name")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
+
+const ID = document.getElementById("ID");
+const Nombre = document.getElementById("Nonbres");
+const Apellido = document.getElementById("Apellidos");
+const Telefono= document.getElementById("Numero de Telefono");
+const Fecha  = document.getElementById("Fecha de Nacimiento");
+const email = document.getElementById("Correo");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+
+
+form.addEventListener('submit', e=> {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs(){
+    const IDValue=ID.nodeValue.trim();
+    const NombreValue=Nombre.nodeValue.trim();
+    const ApellidoValue=Apellido.nodeValue.trim();
+    const TelefonoValue=Telefono.nodeValue.trim();
+    const FechaValue=Fecha.nodeValue.trim();
+    const emailValue=email.nodeValue.trim();
+    const passwordValue=password.nodeValue.trim();
+    const formValue=form.nodeValue.trim();
+
+        if(IDValue===''){
+            setErrorFor(ID,"Obliglatorio")
+
+        }
+}
+
+function setErrorFor(input,message){
+    const formControl=input.parentElemet;
+
+
+
+function setErrorFor(input,message){
+    const formControl=input.parentElemet;
+}
+
+function Enviar(){
+
+
+        document.write("Registrado Exitosamente");
+    
+}
+}
